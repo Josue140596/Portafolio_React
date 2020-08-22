327adb7e77829ce45fd5aa42a821ffdf40a728c8
Bryan <josue140596@gmail.com>
2020-08-21 23:53:40 -0500

    first commit

---
+++ docs/precache-manifest.434a2312d56456782838943726cb6fb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39effe5cbf675dec5cd57e3b2e566d56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "971c555129a439db566f",
+    "url": "/static/css/main.bf387109.chunk.css"
+  },
+  {
+    "revision": "a2f27ada284abc33f3f0",
+    "url": "/static/js/2.8837353c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8837353c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "971c555129a439db566f",
+    "url": "/static/js/main.fdc4cccc.chunk.js"
+  },
+  {
+    "revision": "6def91fe5617fa25a8a1",
+    "url": "/static/js/runtime-main.3f42932f.js"
+  }
+]);
